bf7d1ee1e6dab33e4a307e85453f918a9bac0efa
kadhir <kkadhir10@gmail.com>
2016-01-13 14:19:37 +0530

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gyff-web
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/HomeController.js
@@ -0,0 +1,16 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing homes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	'gallary' : function (req, res) {
+		res.view()
+	},
+	'contact-us' : function (req, res) {
+		res.view()
+	},
+};
+
